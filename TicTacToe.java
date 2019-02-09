8b87f99db298f4074f66bcf2508cb7c6a522e848
Leonardo Tse <leonardo.tse@gmail.com>
2019-02-09 15:44:12 -0800

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -21,7 +21,7 @@ public class TicTacToe
     {
         // (1) write your code here
         game_board = new String[9];
-        for(int i=0; i<9; i++){
+        for(int i=0; i<10; i++){
             game_board[i] = String.valueOf(i+1); //turns the numbers into strings, populating game_board
         }
     }
@@ -37,7 +37,6 @@ public class TicTacToe
         System.out.println("| " + game_board[6] + " | " + game_board[7] + " | " + game_board[8] + " |");
         System.out.println("/---|---|---\\");
 
-
     }
 
     static String checkWinner()
@@ -48,7 +47,7 @@ public class TicTacToe
             switch (a)
             {
                 // (5) write your code here
-
+                case 0:
             }
 
             // (6) write the condition to return the winner
@@ -75,12 +74,15 @@ public class TicTacToe
         whose_turn = "X";
         String winner = null;
 
+
+        System.out.println("Welcome to 2 Player Tic Tac Toe.");
+        System.out.println("--------------------------------");
         populateEmptyBoard(); // (1) use this method to populate the empty board
 
 
         printBoard(); // (2) use this method to print the Tic Tac Toe board as showed in sample output
 
-
+        System.out.println("X's will play first. Enter a slot to place X in.");
 
         // Logic to check if the number entered is in range or not and to check who is winner
         while (winner == null)
@@ -98,10 +100,10 @@ public class TicTacToe
                 System.out.println("Invalid input; Re start the game");
             }
 
-            if (game_board[number-1]==number))  // (3) This statement is incorrect, edit it
+            if (game_board[number-1] != "X" || game_board[number-1] != "O")  // (3) This statement is incorrect, edit it
             {
                 // (4) write the code here for assigning 'X' or 'O' and changing turns.
-
+                game_board[number-1] = whose_turn;
 
                 printBoard(); // (2) use this method to print the Tic Tac Toe board as showed in sample output
 
